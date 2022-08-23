4d30b10d1ed41a6d1ef31a84f7b9d93954be3139
Rafael Alves <rbalves.ads@gmail.com>
2022-08-23 20:31:53 -0300

    feat: add bottom navigation

--- App.js
+++ App.js
@@ -1,13 +1,42 @@
+import { useState } from "react";
+
+import { BottomNavigation, Text } from "react-native-paper";
+
 import { ApolloProvider } from "@apollo/client";
 
 import { client } from "./src/services/graphql/client";
 
 import Home from "./src/components/Home";
+import Groups from "./src/components/Groups";
 
 export default function App() {
+  const [index, setIndex] = useState(0);
+
+  const [routes] = useState([
+    {
+      key: "teams",
+      title: "Teams",
+    },
+    {
+      key: "groups",
+      title: "Groups",
+    },
+  ]);
+
+  const renderScene = BottomNavigation.SceneMap({
+    teams: () => <Home />,
+    groups: () => <Groups />,
+  });
+
   return (
     <ApolloProvider client={client}>
-      <Home />
+      <BottomNavigation
+        navigationState={{ index, routes }}
+        onIndexChange={setIndex}
+        renderScene={renderScene}
+        barStyle={{ backgroundColor: "#AA4A44" }}
+        safeAreaInsets={{ top: 0, bottom: 12, left: 0, right: 0 }}
+      />
     </ApolloProvider>
   );
 }
